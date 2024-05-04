f12585b054cde595b6c065dabe9c0700cbc8a42c
Nhut Tran <minhnhut37@gmail.com>
2024-05-05 01:25:36 +0700

    Initial version

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,11 @@
-import { NativeScriptConfig } from '@nativescript/core';
+import { NativeScriptConfig } from "@nativescript/core";
 
 export default {
-  id: 'org.nativescript.poolparty',
-  appPath: 'src',
-  appResourcesPath: 'App_Resources',
+  id: "net.newbiescripter.poolparty.app",
+  appPath: "src",
+  appResourcesPath: "App_Resources",
   android: {
-    v8Flags: '--expose_gc',
-    markingMode: 'none'
-  }
+    v8Flags: "--expose_gc",
+    markingMode: "none",
+  },
 } as NativeScriptConfig;
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,64 +1,36 @@
 <script lang="ts" setup>
-import {
-  ref,
-  computed,
-  onMounted,
-  onUnmounted,
-  $navigateTo,
-} from 'nativescript-vue';
-import Details from './Details.vue';
+import { onMounted, onUnmounted } from "nativescript-vue";
 
-const counter = ref(0);
-const message = computed(() => {
-  return `Blank {N}-Vue app: ${counter.value}`;
-});
+onMounted(() => {});
 
-function logMessage() {
-  console.log('You have tapped the message!');
-}
-
-let interval: any;
-onMounted(() => {
-  console.log('mounted');
-  interval = setInterval(() => counter.value++, 100);
-});
-
-onUnmounted(() => {
-  console.log('unmounted');
-  clearInterval(interval);
-});
+onUnmounted(() => {});
 </script>
 
 <template>
   <Frame>
-    <Page>
-      <ActionBar>
-        <Label text="Home" class="font-bold text-lg" />
+    <Page class="bg-orange-100">
+      <ActionBar class="bg-zinc-800">
+        <GridLayout rows="37, 60" columns="40,*,40" width="100%">
+          <Image
+            class="bg-orange-100"
+            src="~/assets/top.png"
+            col="0"
+            colSpan="3"
+            row="0"
+            height="37"
+            width="100%"
+            stretch="fill"
+          />
+          <Label
+            class="text-center font-bold text-xs color-orange-100"
+            col="1"
+            row="1"
+            text="Poolparty FM"
+          />
+        </GridLayout>
       </ActionBar>
-
-      <GridLayout rows="*, auto, auto, *" class="px-4">
-        <Label
-          row="1"
-          class="text-xl align-middle text-center text-gray-500"
-          :text="message"
-          @tap="logMessage"
-        />
-
-        <Button
-          row="2"
-          @tap="$navigateTo(Details)"
-          class="mt-4 px-4 py-2 bg-white border-2 border-blue-400 rounded-lg"
-          horizontalAlignment="center"
-        >
-          View Details
-        </Button>
-      </GridLayout>
     </Page>
   </Frame>
 </template>
 
-<style>
-/* .info {
-    font-size: 20;
-  } */
-</style>
+<style></style>
