88f6791d8dfbb1dc993056a78234d6fe2ab9ec15
Nhut Tran <minhnhut37@gmail.com>
2024-05-05 15:15:37 +0700

    Feat: - Channel selector - Progress bar

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
-import { onMounted, onUnmounted } from "nativescript-vue";
+import { onMounted, onUnmounted, ref } from "nativescript-vue";
 
-onMounted(() => {});
+// define a variable
+let progress = ref(0.0); // in percent
+let timer: any;
+onMounted(() => {
+  timer = setInterval(() => {
+    progress.value += 0.3;
+    if (progress.value >= 100) {
+      clearInterval(timer);
+    }
+  }, 100);
+});
 
-onUnmounted(() => {});
+onUnmounted(() => {
+  clearInterval(timer);
+});
 </script>
 
 <template>
@@ -31,10 +43,61 @@ onUnmounted(() => {});
             class="fas font-bold text-xs color-orange-100"
             col="2"
             row="1"
-            :text="$fonticon('fa-angle-right')"
+            :text="$fonticon('fa-arrow-right-long')"
           />
         </GridLayout>
       </ActionBar>
+
+      <StackLayout class="p-2">
+        <GridLayout
+          class="pt-3 bg-zinc-800 rounded"
+          columns="80,*,80"
+          rows="50, 70"
+          height="auto"
+        >
+          <Button
+            class="fas text-xs bg-zinc-800"
+            color="#fff"
+            col="0"
+            row="0"
+            :text="$fonticon('fa-caret-left')"
+          />
+
+          <Button
+            class="text-sm border border-r-4 border-b-4 border-white rounded bg-zinc-800"
+            color="#fff"
+            col="1"
+            row="0"
+            height="40"
+            text="$fonticon('fa-angle-left')"
+          >
+            <FormattedString>
+              <Span text="Channel: " />
+              <Span class="font-bold" text="Poolsuite FM" />
+              <Span text="    " />
+              <Span class="fas text-xs" :text="$fonticon('fa-sort')" />
+            </FormattedString>
+          </Button>
+
+          <Button
+            class="fas text-xs bg-zinc-800"
+            color="#fff"
+            col="2"
+            row="0"
+            :text="$fonticon('fa-caret-right')"
+          />
+
+          <AbsoluteLayout colSpan="3" row="1" col="0" class="mt-3 bg-zinc-700">
+            <StackLayout
+              class="bg-zinc-600"
+              height="70"
+              top="0"
+              left="0"
+              :width="progress + '%'"
+            />
+          </AbsoluteLayout>
+        </GridLayout>
+      </StackLayout>
     </Page>
   </Frame>
 </template>
