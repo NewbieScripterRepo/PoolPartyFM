66c8d94880174618c0a00a3f3267e46b402af2bd
Nhut Tran <minhnhut37@gmail.com>
2024-05-05 16:26:24 +0700

    Feat: - Add play control

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,4 +1,5 @@
 <script lang="ts" setup>
+import { FormattedString } from "@nativescript/core";
 import { onMounted, onUnmounted, ref } from "nativescript-vue";
 
 // define a variable
@@ -96,6 +97,90 @@ onUnmounted(() => {
               :width="progress + '%'"
             />
           </AbsoluteLayout>
+        </GridLayout>
+
+        <!-- Play control -->
+        <GridLayout
+          class="bg-amber-100 border-4 border-black rounded-b"
+          rows="80, 100, 90"
+          columns="65,*,*,65"
+        >
+          <Button
+            class="fas text-sm bg-transparent z-0"
+            color="#000"
+            col="0"
+            row="0"
+            :text="$fonticon('fa-arrow-up-from-bracket')"
+          />
+          <Button
+            class="fa text-sm bg-transparent z-0"
+            color="#000"
+            col="3"
+            row="0"
+            :text="$fonticon('fa-heart')"
+          />
+
+          <StackLayout row="0" col="1" colSpan="2" class="py-2 color-black">
+            <Label class="text-xs text-center" text="0:00 / 3:47" />
+            <Label
+              class="font-bold text-sm text-center"
+              text="High Hopes [SOS Band Cover]"
+            />
+            <Label class="text-xs text-center" text="Basement Love" />
+          </StackLayout>
+
+          <GridLayout
+            class="mx-4 mt-8 border-2 border-r-8 border-b-8 border-black rounded"
+            row="1"
+            col="0"
+            colSpan="4"
+            columns="*,*,*"
+            height="auto"
+          >
+            <Button
+              class="fas text-sm bg-transparent z-0 border-r-2 border-black"
+              color="#000"
+              col="0"
+              row="0"
+              :text="$fonticon('fa-step-backward')"
+            />
+            <Button
+              class="fas text-sm bg-transparent z-0"
+              color="#000"
+              col="1"
+              row="0"
+              :text="$fonticon('fa-play')"
+            />
+            <Button
+              class="fas text-sm bg-transparent z-0 border-l-2 border-black"
+              color="#000"
+              col="2"
+              row="0"
+              :text="$fonticon('fa-step-forward')"
+            />
+          </GridLayout>
+
+          <Button
+            class="text-right text-sm pr-4 bg-transparent z-0"
+            row="2"
+            col="1"
+          >
+            <FormattedString>
+              <Span class="fa" :text="$fonticon('fa-paper-plane')" />
+              <Span text=" Airplay" />
+            </FormattedString>
+          </Button>
+
+          <Button
+            class="text-left text-sm pl-4 bg-transparent z-0"
+            row="2"
+            col="2"
+          >
+            <FormattedString>
+              <Span class="fa" :text="$fonticon('fa-user')" />
+              <Span text=" Sign in" />
+            </FormattedString>
+          </Button>
         </GridLayout>
       </StackLayout>
     </Page>
